b3886031da4a8fff809ac404e5d4dc2b5ad174a5
flavioski <flappio.pelliccia@gmail.com>
2016-07-03 17:38:59 +0200

    debugbar

--- config/app.php
+++ config/app.php
@@ -156,6 +156,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+         * Debugbar
+         */
+        Barryvdh\Debugbar\ServiceProvider::class,
+
     ],
 
     /*
@@ -201,6 +206,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        /**
+         * Debugbar
+         */
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
