d56cec1908133349c7f61433b70642e24910d8da
flavioski <flappio.pelliccia@gmail.com>
2016-07-06 22:56:04 +0200

    head blade

---
+++ resources/views/layouts/frontend/head.blade.php
@@ -0,0 +1,36 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="interno 12 - interior, jewels design & fashion">
+    <meta name="author" content="Chiara Dal Ben Designer">
+
+    <meta property="og:title" content="interno 12" />
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content="interno 12 - interior, jewels design & fashion" />
+    <meta property="og:url" content="http://www.interno12.net/" />
+    <meta property="og:image" content="http://www.interno12.net/img/logo.png" />
+    <meta property="og:image" content="http://www.interno12.net/img/logo600x600.png" />
+    <meta property="og:image" content="http://www.interno12.net/img/logo600x314.png" />
+    <meta property="og:sitename" content="www.interno12.net" />
+    <meta property="og:url" content="http://www.interno12.net" />
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@acachawiki" />
+    <meta name="twitter:creator" content="@acacha1" />
+
+    <title>interno 12 - interior, jewels design & fashion</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{ asset('/css/bootstrap.css') }}" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('/css/main.css') }}" rel="stylesheet">
+
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+
+    <script src="{{ asset('/plugins/jQuery/jQuery-2.1.4.min.js') }}"></script>
+    <script src="{{ asset('/js/smoothscroll.js') }}"></script>
+
+
+</head>
--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -3,43 +3,7 @@
 Landing page based on Pratt: http://blacktie.co/demo/pratt/
 -->
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="interno 12 - interior, jewels design & fashion">
-    <meta name="author" content="Chiara Dal Ben Designer">
-
-    <meta property="og:title" content="interno 12" />
-    <meta property="og:type" content="website" />
-    <meta property="og:description" content="interno 12 - interior, jewels design & fashion" />
-    <meta property="og:url" content="http://www.interno12.net/" />
-    <meta property="og:image" content="http://www.interno12.net/img/logo.png" />
-    <meta property="og:image" content="http://www.interno12.net/img/logo600x600.png" />
-    <meta property="og:image" content="http://www.interno12.net/img/logo600x314.png" />
-    <meta property="og:sitename" content="www.interno12.net" />
-    <meta property="og:url" content="http://www.interno12.net" />
-
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:site" content="@acachawiki" />
-    <meta name="twitter:creator" content="@acacha1" />
-
-    <title>interno 12 - interior, jewels design & fashion</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="{{ asset('/css/bootstrap.css') }}" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="{{ asset('/css/main.css') }}" rel="stylesheet">
-
-    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
-
-    <script src="{{ asset('/plugins/jQuery/jQuery-2.1.4.min.js') }}"></script>
-    <script src="{{ asset('/js/smoothscroll.js') }}"></script>
-
-
-</head>
-
+@extends('layouts.frontend.head')
 <body data-spy="scroll" data-offset="0" data-target="#navigation">
 
 <!-- Fixed navbar -->
