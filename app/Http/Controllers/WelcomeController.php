44acad133fe1894994fb6e7e9e1b4ef5e5e9a180
flavioski <flappio.pelliccia@gmail.com>
2016-07-10 11:57:55 +0200

    middleware web

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -17,16 +17,6 @@ use Illuminate\Http\Request;
 class WelcomeController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
      * Show the application dashboard.
      *
      * @return Response
